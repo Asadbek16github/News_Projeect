a995a211502873a396abfd946999efaf8f071353
Asadbek Shomurodov <110631946+Asadbek16github@users.noreply.github.com>
2023-04-24 18:53:48 +0500

    Parolni almashtirish sahifasi qo'shildi

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from .views import dashboard
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login_page'),
     path('logout/', LogoutView.as_view(), name='logout_page'),
-    path('dashboard/', dashboard, name='dashboard_page')
+    path('dashboard/', dashboard, name='dashboard_page'),
+    path('passwordChange/', PasswordChangeView.as_view(), name='password_change_page'),
+    path('passwordChangeDone/', PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
